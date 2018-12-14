a4058ef579a86dec91132472a1e3f25046f9d532
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 11:05:02 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,27 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	//	Scanner s=new Scanner(System.in);
+		int a=17;int b=17;
+		if(a<b)
+		{
+			System.out.println("a is smaller : "+a+"\nb is larger : "+b);
+			
+		}
+		if(b<a)
+		{
+			System.out.print("b is smaller : "+b+"\na is larger : "+a);
+		}
+		if(a==b)
+		System.out.print("Both are equal");
+		
+	}
+}
