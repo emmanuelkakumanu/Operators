34c883571a4943dd033a21402280632a0f3d7dd8
emmanuelkakumanu <41730018+emmanuelkakumanu@users.noreply.github.com>
2018-12-14 10:58:53 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int a=7,b=10,c=10,d=7;
+		System.out.println("a>b : "+(a>b));
+		System.out.println("a<b : "+(a<b));
+		System.out.println("a>=d : "+(a>=d));
+		System.out.println("b<=c : "+(b<=c));
+	}
+}
